bdbdeb96cd99238b2f6ceccecffa5b577d73ac9e
jackson-barnes <jackson.barnes162@gmail.com>
2026-07-23 10:49:48 +0100

    Initial Commit

---
+++ MidniteTask/MidniteTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidniteTaskApp.swift
+//  MidniteTask
+//
+//  Created by pina lina on 22/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct MidniteTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
